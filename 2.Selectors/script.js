5540e39caed42d92c2eac18754d746ed5010d780
jordydp <jdepril@outlook.com>
2022-01-10 16:38:49 +0100

    ex 2 ( undisplay img with no classname important)

--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -1,8 +1,15 @@
-const selected = document.querySelectorAll(".important") //create array with all Important class items
-for(item = "0"; item < selected.length ; item ++){ //array overlopen voor de waarde van Important
-    selected[item].setAttribute("title", "this is an important item")// alle items met important title toevoegen
-    
+const selected = document.querySelectorAll(".important");//create array with all Important class items
+for( let item = 0 ; item < selected.length ; item ++){ //array overlopen voor de waarde van Important
+    selected[item].setAttribute("title", "this is an important item");// alle items met important title toevoegen
+    console.log(selected[item]);
 }
 
 
 
+const img = document.querySelectorAll('img'); //look for every img in document
+console.log(img); //display all img in console
+for( let i = 0 ; i < img.length ; i ++){ //check array starting from 0 and keep checking till last img 
+    if(img[i].className != "important"){ // check if item in array classname not equal to "important"
+       img[i].style.display="none"; // if the image has no classname of important change the display value to none
+    }
+}
