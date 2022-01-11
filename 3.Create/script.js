33763434637f61cd54e7aa99e45cd9590bfa1a88
jordydp <jdepril@outlook.com>
2022-01-11 15:49:35 +0100

    text color reverse bg color

--- 3.Create/script.js
+++ 3.Create/script.js
@@ -1,4 +1,6 @@
-const names =[ "Ahmad","Jian", "Michael", "Dante", "Muhammet", "Nicolas", "Beryl", "Cynthia","Ruben","Magali", "Jawid","Koen","Kevin","Benjamin","Dery","Michelle", "colin","Feruz", "Pierter", "Ellen", "Sara"]
+const names =[ "Ahmad","Jian", "Michael", "Dante", "Muhammet", "Nicolas", "Beryl", "Cynthia","Ruben","Magali", "Jawid","Koen","Kevin","Benjamin","Dery","Michelle", "colin","Feruz", "Pierter", "Ellen", "Sara"];
+const shuffledPeople = names.sort((a, b) => 0.5 - Math.random());
+
 function random_bg_color() { //create function
     var x = Math.floor(Math.random() * 256); //generate 1ste rbg color (Var x  Math.floor =
     var y = Math.floor(Math.random() * 256);//generate 2nd rbg color
@@ -6,11 +8,22 @@ function random_bg_color() { //create function
     var bgColor = "rgb(" + x + "," + y + "," + z + ")"; // putting x y z on the respective place in rbg
  console.log(bgColor);// display rgb
  return bgColor;    
+}
+
+function getTextColor(rgba) {
+    rgba = rgba.match(/\d+/g);
+    if((rgba[0]*0.299)+(rgba[1]*0.587)+(rgba[2]*0.114)>127) {
+      return 'black';
+    } else {
+      return 'white';
     }
+  }
+
 function addSection(name) {
     const newSec = document.createElement("section"); // create a section
     newSec.style.backgroundColor = random_bg_color(); //give bgcolor to section
-    const art = document.querySelector("article"); // loook for article
+    newSec.style.color= getTextColor(newSec.style.backgroundColor); // uses function on color of newSec.style.backgroundColor
+    const art = document.querySelector("article"); // look for article
     art.appendChild(newSec); //append newSec to article    
     const heading = document.createElement("p"); // create p element
     newSec.appendChild(heading); //adding p element to section
@@ -23,9 +36,11 @@ for(let i=0; i < names.length ; i++){
     const name = names[i];//declare var for items in array
     addSection(name);//use function for each name
     
+    
 
 
 }
+
 /*names.forEach((name) => addSection(name));
 names.forEach((name) => {
 
