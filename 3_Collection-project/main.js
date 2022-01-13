536b0c9baf1745dab637a798f4cd0241416e7008
jordydp <jdepril@outlook.com>
2022-01-13 15:18:22 +0100

    split up js files

---
+++ 3_Collection-project/main.js
@@ -0,0 +1,101 @@
+import { collection } from "./collection.js";
+
+
+for( let i = 0; i < collection.length ; i++){
+    //create a new div
+    const newDiv = document.createElement("div");
+    // add classname to div
+    newDiv.className ="card"; 
+    //apend to right row in function of position
+    if(collection[i].position === "Striker"){
+        let rowselctor = document.querySelector(".row-attack");
+        rowselctor.appendChild(newDiv);
+    }
+    else if(collection[i].position === "Midfielder"){
+        let rowselctor = document.querySelector(".row-midfield");
+        rowselctor.appendChild(newDiv);
+    }
+    else if(collection[i].position === "Defender"){
+        let rowselctor = document.querySelector(".row-defence");
+        rowselctor.appendChild(newDiv);
+    }
+    else if(collection[i].position === "Goalkeeper"){
+        let rowselctor = document.querySelector(".row-goalie");
+        rowselctor.appendChild(newDiv);
+    }
+    else{
+        let rowselctor = document.querySelector(".row-sub");
+        rowselctor.appendChild(newDiv);
+    }
+    
+    const newTitle = document.createElement("h1");//create h1 for name
+    newTitle.className= "Title"
+    newDiv.appendChild(newTitle);//append name in newdiv
+    newTitle.innerHTML = collection[i].name;// display name in h1
+    
+    const newPCountry = document.createElement("p"); //create p 
+    newPCountry.className="Country"
+    newDiv.appendChild(newPCountry); // append p into newDiv
+    newPCountry.innerHTML = "Country:"+ " " + collection[i].nationality;//give text to p
+    
+    const newPComp = document.createElement("p"); //create p 
+    newPComp.className="Competition";
+    newDiv.appendChild(newPComp); // append p into newDiv
+    newPComp.innerHTML = "Competition:" + " " + collection[i].competition;//give text to p
+    
+    const newPClub = document.createElement("p")
+    newPClub.className = "Club";
+    newDiv.appendChild(newPClub);
+    newPClub.innerHTML = "Club:" + " " + collection[i].club;
+    
+    const newPAge = document.createElement("p");
+    newPAge.className= "age";
+    newDiv.appendChild(newPAge);
+    newPAge.innerHTML = "Born in:"+ " " + collection[i].age;
+    
+    const newPPosition = document.createElement("p");
+    newPPosition.className = "Position";
+    newDiv.appendChild(newPPosition);
+    newPPosition.innerHTML ="Position:" + " " + collection[i].position;
+    
+    const newPFoot = document.createElement("p");
+    newPFoot.className= "Foot";
+    newDiv.appendChild(newPFoot);
+    newPFoot.innerHTML = "Preferred foot:" + " " + collection[i].foot;
+    
+    
+    
+    let align = document.getElementsByClassName("Foot"); //get element foot
+    
+    // create if/else function for value of Foot If value of foot is Right
+    if(collection[i].foot === "Right"){
+    
+        // align item right
+        align[i].style.textAlign ="right";
+    
+        //align item left
+    } else{
+        align[i].style.textAlign ="left";
+    }
+    
+    //create bg-color for every possition
+    
+    if(collection[i].position === "Striker"){
+        newDiv.style.border = "2px solid blue";
+    }
+    else if(collection[i].position === "Midfielder"){
+        newDiv.style.border = "2px solid yellow";
+    }
+    else if(collection[i].position === "Defender"){
+        newDiv.style.border = "2px solid brown";
+    }
+    else if(collection[i].position === "Goalkeeper"){
+        newDiv.style.border = "2px solid pink";
+    }
+    else{
+        newDiv.style.border = "2px solid gold";
+    }
+    newDiv.style.backgroundImage = "url(" + collection[i].picture +")";
+    
+    }
+    
