b111441b779c35fc91f8c0259e337959983a5522
jordydp <jdepril@outlook.com>
2022-01-17 11:34:18 +0100

    exc 4 Finishd

--- 3_Collection-project/4.Navigate/script.js
+++ 3_Collection-project/4.Navigate/script.js
@@ -5,9 +5,39 @@
 */
 
 //Select the last child of the <ol> and put it in the beginning of the list
-
-const ol = document.querySelector("ol");
-const a = ol.children[0];
+//select ol
+const ol = document.querySelector("ol"); 
+// a = first child of ol
+const a = ol.children[0]; 
+// b = last child of ol
 const b = ol.children[4];
+//re-arange b before a 
 ol.insertBefore(b,a);
+
+//move H2 of the third section in the second one and vice versa
+
+//select all section
+
+const section = document.querySelectorAll("section");
+// select first section children
+const sectionChildren = section[0].children;
+
+//select h2 in firstt section
+const h2 = section[0].children[0];
+//select h2 in second section
+const h2Sec1 = section[1].children[0];
+//append h2 from section1 to section 2
+section[1].appendChild(h2);
+//append h2 from section 2 to section 1
+section[0].appendChild(h2Sec1);
+
+
+//Delete the last section from the DOM, we don't need it anyways
+
+const Section3 = section[2];
+Section3.remove();
+
+
+
+
 
